919fc30ed2c2ac74f5ba2314fa3b65be4e123241
Yuri Beliakov <yuri.beliakov@gmail.com>
2018-05-24 18:34:25 +0300

    Update test

--- search_google_test.py
+++ search_google_test.py
@@ -2,21 +2,26 @@ import pytest
 import time
 import datetime
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 
 
 @pytest.fixture
 def driver(request):
-    chromeOptions = webdriver.ChromeOptions()
-    # chromeOptions.add_argument("--start-fullscreen")
-    wd = webdriver.Chrome('./chromedriver', options=chromeOptions)
+    browser_options = webdriver.ChromeOptions()
+    browser_options.add_argument("--start-maximized")
 
-    # wd = webdriver.Safari()
-    wd.maximize_window()
-    print(wd.capabilities)
-    request.addfinalizer(wd.quit)
-    return wd
+    browser = webdriver.Chrome(options=browser_options)
+    # browser = webdriver.Safari()
+
+    main_window = browser.current_window_handle
+    browser.switch_to.window(main_window)
+    browser.maximize_window()
+
+    print(browser.capabilities)
+    request.addfinalizer(browser.quit)
+    return browser
 
 
 def test_google_search(driver):
@@ -24,11 +29,17 @@ def test_google_search(driver):
     driver.find_element_by_name("q").send_keys("webdriver")
     search_box = driver.find_element_by_name('q')
     search_box.submit()
-    WebDriverWait(driver, 1).until(EC.title_is("webdriver - Поиск в Google"))
+    WebDriverWait(driver, 1).until(ec.title_is("webdriver - Поиск в Google"))
+
+    time.sleep(1)
+
 
 def test_add_project(driver):
     driver.get("http://www.shipovalov.net")
+
     driver.find_element_by_name("username").send_keys("student")
+    driver.find_element_by_name("username").send_keys(Keys.HOME)
+    driver.find_element_by_name("username").send_keys(Keys.COMMAND, "A")
     driver.find_element_by_name("password").send_keys("luxoft")
     driver.find_element_by_css_selector(".button").click()
 
@@ -40,4 +51,4 @@ def test_add_project(driver):
     driver.find_element_by_name("description").send_keys("Description")
     driver.find_element_by_css_selector(".button").click()
 
-    time.sleep(10)
+    time.sleep(1)
